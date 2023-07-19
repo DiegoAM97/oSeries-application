1b1631a682f3842b6f00982f5fc67551dc98a38f
DiegoAM97 <diegotar2@gmail.com>
2023-07-19 19:07:08 +0200

    Add the screen scraping for series

--- data-scraping.py
+++ data-scraping.py
@@ -5,26 +5,29 @@ BASE_URL = "https://www.themoviedb.org"
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 
-def retrieveMovieData(moviePath):
+def retrieveData(path):
 
-    url = BASE_URL + moviePath
+    url = BASE_URL + path
     movieContent = requests.get(url, headers=HEADERS)
     soup = BeautifulSoup(movieContent.text, "html.parser")
-    
-    title = soup.find("section", {"class" : "header poster"}).find("a")
-    releaseDate = soup.find("section", {"class" : "header poster"}).find("span", {"class" : "release"})
-    duration = soup.find("section", {"class" : "header poster"}).find("span", {"class" : "runtime"})
-    score = soup.find("section", {"class" : "header poster"}).find("div", {"class" : "user_score_chart"})["data-percent"]
-    genres = soup.find("section", {"class" : "header poster"}).find("span", {"class" : "genres"})
-    tagLine = soup.find("section", {"class" : "header poster"}).find("h3", {"class" : "tagline"})
-    overview = soup.find("section", {"class" : "header poster"}).find("div", {"class" : "overview"})
-    director = soup.find("section", {"class" : "header poster"}).find("li", {"class" : "profile"}).find("a")
-    budget = soup.find("section", {"class" : "facts"}).find_all("p", {"class" : None})[2].text.split(" ")[1]
-    revenue = soup.find("section", {"class" : "facts"}).find_all("p", {"class" : None})[3].text.split(" ")[1]
 
-    # Some movies have no video in the web page
-    video = soup.find("section", {"class" : "header poster"}).find("li", {"class" : "video"})
-    video = video.find("a")["data-id"] if video is not None else ''
+    posterSection = soup.find("section", {"class" : "header poster"})
+    factsSection = soup.find("section", {"class" : "facts"}).find_all("p", {"class" : None})
+
+    title = posterSection.find("a")
+    releaseDate = posterSection.find("span", {"class" : "release"})
+    duration = posterSection.find("span", {"class" : "runtime"})
+    score = posterSection.find("div", {"class" : "user_score_chart"})["data-percent"]
+    genres = posterSection.find("span", {"class" : "genres"})
+    tagLine = posterSection.find("h3", {"class" : "tagline"})
+    overview = posterSection.find("div", {"class" : "overview"})
+    profile = posterSection.find("li", {"class" : "profile"})
+    director = posterSection.find("li", {"class" : "profile"}).find("a") if profile is not None else None
+    budget = factsSection[2].text.split(" ")[1] if len(factsSection) >= 4 else ""
+    revenue = factsSection[3].text.split(" ")[1] if len(factsSection) >= 4 else ""
+
+    video = posterSection.find("li", {"class" : "video"})
+    video = video.find("a")["data-id"] if video is not None else ""
     
     img = soup.find("img", {"class" : "poster"})["src"]
     res = requests.get((BASE_URL + img).replace("_filter(blur)", "")
@@ -36,16 +39,16 @@ def retrieveMovieData(moviePath):
         img = res.raw
 
 
-    print (f"""Tittle: {title.text.strip() if title is not None else ""} 
-            Release date: {releaseDate.text.strip() if releaseDate is not None else ""}
-            Duration: {duration.text.strip() if duration is not None else ""}
-            Score: {score if score is not None else ""}
-            Genres: {genres.text.strip() if genres is not None else ""}
-            Tagline: {tagLine.text.strip() if tagLine is not None else ""}
-            Overview: {overview.text.strip() if overview is not None else ""}
-            Director: {director.text.strip() if director is not None else ""}
-            Budget: {budget if budget is not None else ""}
-            Revenue: {revenue if revenue is not None else ""}
+    print (f"""Title: {extractText(title)} 
+            Release date: {extractText(releaseDate)}
+            Duration: {extractText(duration)}
+            Score: {score}
+            Genres: {extractText(genres)}
+            Tagline: {extractText(tagLine)}
+            Overview: {extractText(overview)}
+            Director: {extractText(director)}
+            Budget: {budget}
+            Revenue: {revenue}
             VideoId: {video}
             Image: {img}
             """)
@@ -58,10 +61,26 @@ def retrieveTopRatedMoviesByPageParam(pageNumber):
     cardContent = soup.find_all("div", {"class":"card style_1"})
 
     for card in cardContent:
-        retrieveMovieData(card.find("a")["href"])
+        retrieveData(card.find("a")["href"])
+
+
+def retrieveTopRatedSeriesByPageParam(pageNumber):
+
+    url = BASE_URL + f"/tv/top-rated?page={pageNumber}"
+    pageContent = requests.get(url, headers=HEADERS)
+    soup = BeautifulSoup(pageContent.text, "html.parser")
+    cardContent = soup.find_all("div", {"class":"card style_1"})
+
+    for card in cardContent:
+        retrieveData(card.find("a")["href"])
+
+
+def extractText(value):
+    return value.text.strip() if value is not None else ""
 
 
 # Loop through the pages
 
 for i in range(1, 2):
     retrieveTopRatedMoviesByPageParam(i)
+    retrieveTopRatedSeriesByPageParam(i)
